52265d6967798dc7496954cc34ed00692243b463
Vizper02 <74756131+Vizper02@users.noreply.github.com>
2022-12-17 16:55:35 -0700

    characteranimation
    hice el codigo para que tuvieran animacion los personajes

--- christmaswonderland/Assets/scripts/playermovement.cs
+++ christmaswonderland/Assets/scripts/playermovement.cs
@@ -6,10 +6,15 @@ public class playermovement : MonoBehaviour
 {
     private CharacterController characterController;
 
+    public Animator animator;
     public float speed = 5f;
     public float jumpspeed = 5f;
     [SerializeField]
     private Vector3 move;
+    private float horizontal = 0f;
+    private float vertical = 0f;
+    private bool ch = false; 
+    private bool cv = false;
     private float yspeed = 0f;
 
     // Start is called before the first frame update
@@ -21,9 +26,13 @@ public class playermovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        //jumping
         if (characterController.isGrounded)
         {
-            if(Input.GetButtonDown("Jump"))
+            if (Input.GetButtonDown("Jump"))
             {
                 yspeed = jumpspeed;
             }
@@ -31,9 +40,41 @@ public class playermovement : MonoBehaviour
         {
             yspeed += Physics.gravity.y * Time.deltaTime;
         }
+
+        //applymovement
+        move = new Vector3(horizontal, yspeed, vertical);
         
-        move = new Vector3(Input.GetAxis("Horizontal"), yspeed, Input.GetAxis("Vertical"));
-           
+        //animation
+        if(horizontal != 0f)
+        {
+            ch = true;
+        } else
+        {
+            ch = false;
+        }
+        if(vertical != 0f)
+        {
+            cv = true;
+        } else
+        {
+            cv = false;
+        }
+        if(yspeed > 0f)
+        {
+            animator.SetBool("isjumping", true);
+        } else
+        {
+            animator.SetBool("isjumping", false);
+        }
+        if (ch || cv)
+        {
+            animator.SetFloat("blend", 1);
+        } else
+        {
+            animator.SetFloat("blend", 0);
+        }
+
+
     }
 
     
