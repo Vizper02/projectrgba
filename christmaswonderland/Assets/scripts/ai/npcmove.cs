9ed87170e811710f0b12954f8f5cf759c318f941
Vizper02 <74756131+Vizper02@users.noreply.github.com>
2023-01-05 17:39:21 -0700

    ai npcs
    añadi ai npcs

---
+++ christmaswonderland/Assets/scripts/ai/npcmove.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class npcmove : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public float walkpointrange;
+    public Vector3 walkpoint;
+
+    //[SerializeField] private Transform movePositionTransform;
+
+    float time = 0f;
+    public float timedelay;
+    float randomZ;
+    float randomX;
+    Vector3 initpos;
+    bool walkpointset = false;
+
+    //variables para anim 
+    Vector3 lastpos;
+    Vector3 dif;
+    SpriteRenderer sprite;
+    Animator anim;
+    //
+
+    private void Awake()
+    {
+        //referencia para navmeshagent
+        agent = GetComponent<NavMeshAgent>();
+        //
+
+        initpos = transform.position;
+
+        //para anim
+        lastpos = transform.position;
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        anim = GetComponentInChildren<Animator>();
+        //
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        //agent.destination = movePositionTransform.position;
+
+        //buscar walkpoint
+        if (!walkpointset)
+        {
+            randomZ = Random.Range(-walkpointrange, walkpointrange);
+            randomX = Random.Range(-walkpointrange, walkpointrange);
+
+            walkpoint = new Vector3(initpos.x + randomX, initpos.y, initpos.z + randomZ);
+            agent.destination = walkpoint;
+            walkpointset = true;
+        }
+        //
+
+        //para anim
+        dif = transform.position - lastpos;
+        if (dif.z > 0)
+        {
+            lastpos = transform.position;
+            anim.SetFloat("blend", 1);
+            sprite.flipX = true;
+        }
+        else if (dif.z < 0)
+        {
+            lastpos = transform.position;
+            anim.SetFloat("blend", 1);
+            sprite.flipX = false;
+        }
+        else if (dif.z == 0)
+        { 
+            anim.SetFloat("blend", 0);
+            //
+
+            //wait for delay to start searching for walkpoint again
+            if (time <= timedelay)
+            {
+                time = time + 1f * Time.deltaTime; 
+            } else
+            {
+                walkpointset = false;
+                time = 0f;
+            }
+            //
+        } 
+
+    }
+}
